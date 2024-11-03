3c49726a6726f15b5f03420c59745de8dfd5ba8b
probonopd <probonopd@users.noreply.github.com>
2024-11-03 07:40:55 +0000

    Make more generic

--- AppImage/srb2/build.sh
+++ AppImage/srb2/build.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 set -eux
 
+export VERSION=$(apk --no-cache -X https://dl-cdn.alpinelinux.org/alpine/edge/testing list "$PACKAGE" | grep -v fetch | cut -d- -f2)
+export ARCH=$(uname -m)
+
 src_dir=${SRC_DIR:-/src}
 out_dir=${OUT_DIR:-/out}
-appdir="$out_dir"/srb2.AppDir
+tools_dir=${TOOLS_DIR:-/tools}
+appdir="$out_dir"/"$PACKAGE".AppDir
 
 apk update && apk add file patchelf
 
-"$src_dir"/witchery-compose \
+"$tools_dir"/witchery-compose \
 	-k /etc/apk/keys \
 	-X https://dl-cdn.alpinelinux.org/alpine/edge/main \
 	-X https://dl-cdn.alpinelinux.org/alpine/edge/community \
 	-X https://dl-cdn.alpinelinux.org/alpine/edge/testing \
-	-d srb2 \
+	-d "$PACKAGE" \
 	-d mesa-gl \
 	-d mesa-egl \
 	-d libx11 \
@@ -23,26 +27,33 @@ apk update && apk add file patchelf
 	"$appdir"
 
 chmod 755 "$appdir"
+
+############################################
+
 sed -i 's/Icon=.*/Icon=srb2/' "$appdir"/usr/share/applications/srb2.desktop
 sed -i 's/Exec=.*/Exec=srb2/' "$appdir"/usr/share/applications/srb2.desktop
 ln -s usr/share/pixmaps/srb2.png "$appdir"/srb2.png
 
-"$src_dir"/appimagetool.AppImage --appimage-extract
+############################################
+
+"$tools_dir"/appimagetool.AppImage --appimage-extract
 
 # This old version of patchelf doesn't work with binaries over a certain size
 # https://github.com/NixOS/patchelf/issues/305
 rm squashfs-root/usr/bin/patchelf
 
-squashfs-root/AppRun -s deploy "$appdir"/usr/share/applications/srb2.desktop
+squashfs-root/AppRun -s deploy "$appdir"/usr/share/applications/"$PACKAGE".desktop
 
 # Demangle ld-musl, which appimagetool messes up with patchelf.
 cp /lib/ld-musl-$(uname -m).so.1 "$appdir"/lib/
+
+############################################
 
 # We need to cd to the data directory before starting the game so it can find its files.
 # Would be better to set SRB2WADDIR, but that doesn't work with relative paths due to a bug.
 sed -i 's|exec|cd share/games/SRB2; exec|' "$appdir"/AppRun
 
-export VERSION=$(apk --no-cache -X https://dl-cdn.alpinelinux.org/alpine/edge/testing list srb2 | grep -v fetch | cut -d- -f2)
-export ARCH=$(uname -m)
+############################################
+
 squashfs-root/AppRun "$appdir"
-mv Sonic_Robo_Blast_2*.AppImage "$out_dir"/
+mv *.AppImage "$out_dir"/
