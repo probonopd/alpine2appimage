25850d9aa517eb04744cce467db1dd0d8b2e2314
probonopd <probonopd@users.noreply.github.com>
2024-12-09 12:38:09 +0100

    -resize 128@

--- AppImage/hexchat/build.sh
+++ AppImage/hexchat/build.sh
@@ -33,7 +33,7 @@ find "$appdir"/usr/share/applications || true
 # ImageMagick even fails to convert the svg, hence we have to grab a png from their website
 apk add imagemagick file
 wget -q "https://avatars.githubusercontent.com/u/1938483?s=200&v=4" -O hexchat.png
-magick -resize 128x128 hexchat.png "$appdir"/usr/share/icons/hicolor/128x128/apps/io.github.Hexchat.png
+magick hexchat.png -resize 128@ "$appdir"/usr/share/icons/hicolor/128x128/apps/io.github.Hexchat.png
 cp "$appdir"/usr/share/icons/hicolor/256x256/apps/io.github.Hexchat.png .
 rm hexchat.png
 
